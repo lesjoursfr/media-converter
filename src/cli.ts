37189cdfe8b2b3cdff00b3534d1b6677e050db99
Adrien ERAUD <e-adrien@users.noreply.github.com>
2025-07-28 09:14:56 +0100

    Migrate yargs to v18

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,13 @@
 import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-export const parse = (argv: readonly string[]) => {
-  return yargs(argv.slice(2))
+export const parse = (argv: string[]) => {
+  return yargs(hideBin(argv))
     .help()
     .alias("help", "h")
     .commandDir("commands", {
       extensions: process.env.NODE_ENV === "development" ? ["js", "ts"] : ["js"],
     })
-    .locale("en").argv;
+    .locale("en")
+    .parse();
 };
